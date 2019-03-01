a46e032315b50cab5619fe1a8fc2bf0b5f55f24a
zzyToon <2210017539@qq.com>
2019-03-01 11:27:23 +0800

    outline work

---
+++ Assets/Code/InGameCode/ObjectTrans.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectTrans : MonoBehaviour {
+
+    Transform curObj;
+    Transform nextObj;
+    bool isChossingObj = false;
+	// Use this for initialization
+	void Start () {
+        curObj = null;
+        nextObj = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SelectObject.MousePick() != null)
+        {
+            ChoosingObj(SelectObject.MousePick());
+        } 
+	}
+
+    public void ChoosingObj(Transform obj)
+    {
+        nextObj = obj;
+        DoOutLine();
+    }
+
+    void DoOutLine()
+    {
+        if (isChossingObj == true)
+        {
+            Debug.Log("CANCLE " + curObj.name);
+            Debug.Log("DRAW " + nextObj.name);
+            curObj.GetComponent<cakeslice.Outline>().color = 0;
+            nextObj.GetComponent<cakeslice.Outline>().color = 1;
+            curObj = nextObj;
+        }
+        else
+        {
+            Debug.Log("NO CALCLE DRAW " + nextObj.name);
+            nextObj.GetComponent<cakeslice.Outline>().color = 1;
+            isChossingObj = true;
+            curObj = nextObj;
+        }
+    }
+}
---
+++ Assets/Code/InGameCode/SelectObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour {
+
+    public static void TouchPick()
+    {
+        if (Input.touchCount != 1)
+            return;
+
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            //射线碰撞信息
+            RaycastHit hit;
+            //射线发射器
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+
+    public static Transform MousePick()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                //Debug.Log(hit.transform.tag);
+                return hit.transform;
+            }
+            return null;
+        }
+        return null;
+    }
+}
