bf49fbd271af2b74b95d98838a362cf9f60322ef
zzyToon <2210017539@qq.com>
2019-03-04 19:19:55 +0800

    3.4 1

---
+++ Assets/Code/InitStageTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitStageTest : MonoBehaviour {
+    public Transform transform;
+    public void Awake()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Instantiate(transform, new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
